c72139ba46d8b2a3df074517dc769c0b10f6a418
Boris Skert <boris.skert@gmail.com>
2021-11-08 15:56:36 +0100

    Provide properties by injection

--- src/main/java/de/borisskert/liberty/rest/ExampleEndpoint.java
+++ src/main/java/de/borisskert/liberty/rest/ExampleEndpoint.java
@@ -1,16 +1,27 @@
 package de.borisskert.liberty.rest;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Optional;
 
 @Path("greeting")
+@RequestScoped
 public class ExampleEndpoint {
+
+    @Inject
+    @ConfigProperty(name = "my.app.property", defaultValue = "Hello World from default value")
+    private String property;
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String getGreeting() {
-        return "Hello World";
+        return Optional.ofNullable(property)
+                .orElse("Hello World from class");
     }
 }
